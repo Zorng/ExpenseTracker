c3d6da566f3c69840c0e5e2518be341aae6b847b
Chey Rotana <rotana.chey@student.cadt.edu.kh>
2025-04-13 21:07:28 +0700

    build record pages

--- src/pages/Records.jsx
+++ src/pages/Records.jsx
@@ -1,34 +1,64 @@
 import Header from "../components/Header.jsx";
 import Filter from "../components/Filter-f.jsx";
 import RecordFolded from "../components/Record-f.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import AddNewRecord from "../components/AddNewRecord(Records).jsx";
+import { RECORDS as initialData } from "../data/data.jsx";
 
 function Records() {
-    // const [filter, setFilter] = useState({
-    //     date: "",
-    //     category: "",
-    //     amount: "",
-    //     currency: "",
-    // });
+    const [filter, setFilter] = useState({
+        date: "",
+        category: "",
+        amount: "",
+        currency: "",
+    });
 
-    // const [appliedFilter, setAppliedFilter] = useState({});
+    const [appliedFilter, setAppliedFilter] = useState({});
+    const [records, setRecords] = useState(() => {
+        const saved = localStorage.getItem("records");
+        const rawRecords = saved ? JSON.parse(saved) : initialData;
+    
+        return rawRecords.map((record) => ({
+            ...record,
+            category:
+                typeof record.category === "string"
+                    ? { name: record.category, color: "#ccc" }
+                    : record.category,
+        }));
+    });    
 
-    // const handleApply = (newFilter) => {
-    //     setAppliedFilter(newFilter);
-    // };
+    useEffect(() => {
+        localStorage.setItem("records", JSON.stringify(records));
+    }, [records]);    
 
-    // return (
-    //     <div>
-    //         <Header />
-    //         <Filter
-    //             filter={filter}
-    //             setFilter={setFilter}
-    //             onApply={handleApply}
-    //         />
-    //         <p className="pl-5 x-h1">Recorded List</p>
-    //         <RecordFolded filter={appliedFilter} />
-    //     </div>
-    // );
+    const handleApply = (newFilter) => {
+        setAppliedFilter(newFilter);
+    };
+
+    return (
+        <div>
+            <header>
+                <Header />
+            </header>
+            <div className="flex flex-col gap-4">
+                <div className="mt-10">
+                    <Filter
+                        filter={filter}
+                        setFilter={setFilter}
+                        onApply={handleApply}
+                    />
+                </div>
+                <div>
+                    <AddNewRecord records={records} setRecords={setRecords} />
+                </div>
+            </div>
+
+            <p className="pl-5 x-h1">Recorded List</p>
+            <div className="w-full">
+                <RecordFolded filter={appliedFilter} records={records} setRecords={setRecords} />
+            </div>
+        </div>
+    );
 }
 
 export default Records;
