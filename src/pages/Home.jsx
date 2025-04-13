a1f584dff2f7c9e2fa478512a9cc97aaec82d7cc
Tong Vorak <vorak.tong@student.cadt.edu.kh>
2025-04-13 23:01:24 +0700

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 import Header from '../components/Header.jsx'
 import AddNewRecord from '../components/AddNewRecord.jsx'
 import Summary from '../components/MonthlyExpenseSummary.jsx'
+import { RecordsProvider } from '../components/RecordContext.jsx'
 
 
 function Home() {
     return (
-        <div>
+        <RecordsProvider> 
             <Header />
             <AddNewRecord />
             <Summary />
-        </div>
+        </RecordsProvider>      
     );
 }
 
